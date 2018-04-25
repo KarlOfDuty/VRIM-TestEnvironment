b0d669760e88373b3e6cab0b4bd640f8c435c8fb
Karl Essinger <xkaess22@gmail.com>
2018-04-26 00:39:15 +0200

    Starting some test mechanics logic

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject cubesParent;
+    public Material highlightedMaterial;
+
+    public GameObject basketTrigger;
+    private GameObject currentCube;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void DeleteCurrentCube()
+    {
+        currentCube.transform.parent = null;
+        SetRandomCube();
+        Destroy(currentCube, 1.0f);
+    }
+
+    private bool SetRandomCube()
+    {
+        if (cubesParent.transform.childCount == 0)
+        {
+            return false;
+        }
+        Transform transform = cubesParent.transform.GetChild(Random.Range(0, cubesParent.transform.childCount));
+        currentCube = transform.gameObject;
+        currentCube.gameObject.GetComponent<Renderer>().material = highlightedMaterial;
+        return true;
+    }
+}
